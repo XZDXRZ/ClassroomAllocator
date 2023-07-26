3d6cdfa524f39c29f1f4731b3acf16958eb38b3d
XZDXRZ <xzdxrz666@gmail.com>
2023-07-26 21:55:32 +1000

    Add and fix the codes

--- ClassroomAllocator/MasterForm.vb
+++ ClassroomAllocator/MasterForm.vb
@@ -12,3 +12,34 @@ Public Class MasterForm
 
 End Class
 
+Enum Period As Integer
+    TUTORIAL = 0
+    PERIOD1
+    PERIOD2
+    RECESS
+    PERIOD3
+    PERIOD4
+    LUNCH
+    PERIOD5
+    PERIOD6
+End Enum
+
+Class Room
+    Public strCode As String
+    Public timePeriod(9) As TimePeriod
+End Class
+
+Class Order
+    Public strApplicantName As String
+    Public strPurpose As String
+End Class
+
+Class TimePeriod
+    Public periodName As Period
+    Public order As Order
+End Class
+
+Class Day
+    Public identifyDate As Date
+    Public rooms(10) As Room
+End Class
