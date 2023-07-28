9a732e366bd0c4bd99d174a0e1064ffa73424a1f
XZDXRZ <xzdxrz666@gmail.com>
2023-07-28 17:37:42 +1000

    Finished reading files

--- ClassroomAllocator/MasterForm.vb
+++ ClassroomAllocator/MasterForm.vb
@@ -9,4 +9,27 @@
 ' ACKNOWLEDGEMENT:
 ' COMMENT:          我永远喜欢爱丽希雅
 
+Imports System.IO
 
+Public Class MasterForm
+    Private Function ReadFromDate(userChooseDate As Date)
+        Dim dateFile As FileStream = New FileStream("../date_files/" & userChooseDate.ToString("ddMMyy") & ".csv", FileMode.OpenOrCreate, FileAccess.ReadWrite)
+
+        dateFile.Close()
+        Return 0
+    End Function
+
+    Private Sub btnLoadTimetable_Click(sender As Object, e As EventArgs) Handles btnLoadTimetable.Click
+        Dim datUserSelectionStart As Date = cldrChooseDate.SelectionStart
+        Dim datUserSelectionEnd As Date = cldrChooseDate.SelectionEnd
+        Dim userSelectionRange As SelectionRange = New SelectionRange(datUserSelectionStart, datUserSelectionEnd)
+
+        If userSelectionRange.Start <> userSelectionRange.End Then
+            MsgBox("Please choose one date", Title:="Choose date Error")
+            Exit Sub
+        End If
+
+        Dim datUserChooseDate As Date = userSelectionRange.Start
+        ReadFromDate(datUserChooseDate.Date)
+    End Sub
+End Class
