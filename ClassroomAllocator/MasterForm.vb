6cac3a2b3487c627013f74da8c856c627c923dd2
XZDXRZ <xzdxrz666@gmail.com>
2023-07-30 17:35:37 +1000

    Finished file reading and processing with full comments

--- ClassroomAllocator/MasterForm.vb
+++ ClassroomAllocator/MasterForm.vb
@@ -13,31 +13,51 @@ Imports System.IO
 
 Public Class MasterForm
     ' A dynamic array containing orders in the selected date
-    Private orders() As Order
+    Private sttOrders() As Order
 
-    Private Sub ReadFromDate(userChooseDate As Date)
+    Private Function ReadFromDate(userChooseDate As Date)
         Try
+            ' A 2-dimensional dynamic array
+            ' 1st dimension: Order Index
+            ' 2nd dimension: 4 data
+            ' Note that all the array starts from 0, ends with total - 1
             Dim strOrders(,) As String
-            Dim strOrdersNoSplit As String
-            Dim strOrdersSplitedByCrlf() As String
+            Dim strOrdersNoSplit As String ' Raw string readed from file
+            Dim strOrdersSplitedByCrlf() As String ' Split raw string by CRLF
 
+            ' Read data from assigned file
             strOrdersNoSplit = My.Computer.FileSystem.ReadAllText("../date_files/" & userChooseDate.ToString("ddMMyy") & ".csv")
+            ' Split raw data by CRLF
             strOrdersSplitedByCrlf = strOrdersNoSplit.Split(vbCrLf)
 
-            Dim intStrOrdersSplitedByCrlfLength As Integer = strOrdersSplitedByCrlf.Length
-            ReDim strOrders(intStrOrdersSplitedByCrlfLength, 4)
+            ' Get the number of row in assigned file
+            Dim intOrdersSplitedByCrlfLength As Integer = strOrdersSplitedByCrlf.Length - 1 ' Last row is always empty
+            ReDim strOrders(intOrdersSplitedByCrlfLength - 1, 3) ' And determine the size of order array
 
-            For orderIndex As Integer = 1 To intStrOrdersSplitedByCrlfLength
-
+            ' Copy data in each row to order array
+            ' Last row is always empty, so read till total - 1
+            For intOrderIndex As Integer = 0 To intOrdersSplitedByCrlfLength - 1
+                ' Split data in each row by comma
+                Dim strTempOrders() As String = strOrdersSplitedByCrlf(intOrderIndex).Split(",")
+                For i As Integer = 0 To 3
+                    strOrders(intOrderIndex, i) = strTempOrders(i)
+                Next
             Next
+
+            ' Return orders in String
+            Return strOrders
         Catch ex As Exception
             ' Create a new file if the file does not exist
             Dim dateFile As FileStream = New FileStream("../date_files/" & userChooseDate.ToString("ddMMyy") & ".csv",
-                       FileMode.Create,
-                       FileAccess.ReadWrite)
+                                                        FileMode.Create,
+                                                        FileAccess.ReadWrite)
             dateFile.Close()
+
+            ' Create and return an empty string array
+            Dim strOrders(0, 0) As String
+            Return strOrders
         End Try
-    End Sub
+    End Function
 
     Private Sub btnLoadTimetable_Click(sender As Object, e As EventArgs) Handles btnLoadTimetable.Click
         ' Judgement of whether user select one date
@@ -51,9 +71,12 @@ Public Class MasterForm
             Exit Sub
         End If
 
+        ' Store the date that user chooses
         Dim datUserChooseDate As Date = userSelectionRange.Start
-        Dim blnFileExists As Boolean
+        Dim strOrders(,) As String
 
-        ReadFromDate(datUserChooseDate.Date)
+        ' Read string data from assigned date, and convert into string array
+        ' Otherwise, create a file if does not exist
+        strOrders = ReadFromDate(datUserChooseDate.Date)
     End Sub
 End Class
