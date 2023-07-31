08d448b3053ddd56db977b8b4a56bf92a3a07288
XZDXRZ <xzdxrz666@gmail.com>
2023-08-01 09:58:51 +1000

    Finished Clear Green Grids Button

--- ClassroomAllocator/MasterForm.vb
+++ ClassroomAllocator/MasterForm.vb
@@ -6,17 +6,16 @@
 ' OUTPUTS:
 ' VERSION HISTORY:  V0.0.1 - 27/07/2023 - GUI nearly done
 '                   V0.0.2 - 31/07/2023 - Major functions done
+'                   V0.1.0 - 01/08/2023 - Finished Basic Functions
 ' COMMENT:          我永远喜欢爱丽希雅
-'                   To
-'                   romantic
-'                   unfailing
-'                   Elysia
+'                   TruE
 
 Imports System.IO
 
 Public Class MasterForm
     ' A dynamic array containing orders in the selected date
     Private sttOrders() As Order
+    Private datCurrentChooseDate As Date
 
     Private Function ReadFromDate(userChooseDate As Date)
         ' A function to read data from given file, or otherwise create a new file.
@@ -63,6 +62,9 @@ Public Class MasterForm
                                                         FileAccess.ReadWrite)
             dateFile.Close()
 
+            ' Inform the user if there is no order on this date
+            MsgBox("Orders loaded, no order on this date", Title:="No Order")
+
             ' Create and return an empty string array
             Dim strOrders(0, 0) As String
             Return strOrders
@@ -88,6 +90,9 @@ Public Class MasterForm
     End Sub
 
     Private Sub btnLoadTimetable_Click(sender As Object, e As EventArgs) Handles btnLoadTimetable.Click
+        ' Clear the table firstly
+        ClearTable()
+
         ' Judgement of whether user select one date
         Dim datUserSelectionStart As Date = cldrChooseDate.SelectionStart
         Dim datUserSelectionEnd As Date = cldrChooseDate.SelectionEnd
@@ -100,13 +105,13 @@ Public Class MasterForm
         End If
 
         ' Store the date that user chooses
-        Dim datUserChooseDate As Date = userSelectionRange.Start
+        datCurrentChooseDate = userSelectionRange.Start
         ' Store the previous data
         Dim strOrders(,) As String
 
         ' Read string data from assigned date, and convert into string array
         ' Otherwise, create a file if does not exist
-        strOrders = ReadFromDate(datUserChooseDate.Date)
+        strOrders = ReadFromDate(datCurrentChooseDate)
         ' Add the data which read previously to sttOrders
         addOrder(strOrders)
         ' Assign value to lblTableLinker
@@ -122,6 +127,11 @@ Public Class MasterForm
     End Sub
 
     Private Sub btnAllocateByTime_Click(sender As Object, e As EventArgs) Handles btnAllocateByTime.Click
+        If cboTimeChoose.SelectedItem Is Nothing Then
+            ' If the users have not choose a time, then inform them
+            MsgBox("Please choose a time period", Title:="No Period Chosen Error")
+            Exit Sub
+        End If
         Dim pedChosePeriod As Period = cboTimeChoose.SelectedItem ' Get the item selected by user
         ' Create a linker between the number pairs and Labels
         Dim linker As LabelTableLinker = New LabelTableLinker()
@@ -134,6 +144,11 @@ Public Class MasterForm
     End Sub
 
     Private Sub btnAllocateByClassroom_Click(sender As Object, e As EventArgs) Handles btnAllocateByClassroom.Click
+        If cboClassroomChoose.SelectedItem Is Nothing Then
+            ' If the users have not choose a room, then inform them
+            MsgBox("Please choose a classroom", Title:="No Room Chosen Error")
+            Exit Sub
+        End If
         Dim rmmChoseRoom As Room = cboClassroomChoose.SelectedItem ' Get the item selected by user
         ' Create a linker between the number pairs and Labels
         Dim linker As LabelTableLinker = New LabelTableLinker()
@@ -155,14 +170,7 @@ Public Class MasterForm
         Next
     End Sub
 
-    Private Sub Clear()
-        ' Clear all text boxes, combo boxes and reset table
-        ' Clear text box
-        txtApplicantNameInput.Text = Nothing
-        txtPurposeInput.Text = Nothing
-        ' Clear combo box
-        cboClassroomChoose.SelectedItem = Nothing
-        cboTimeChoose.SelectedItem = Nothing
+    Private Sub ClearTable()
         ' Create a linker between the number pairs and Labels
         Dim linker As LabelTableLinker = New LabelTableLinker()
         ' Reset table
@@ -178,12 +186,41 @@ Public Class MasterForm
         Next
     End Sub
 
+    Private Sub ClearGreenGrid()
+        ' Create a linker between the number pairs and Labels
+        Dim linker As LabelTableLinker = New LabelTableLinker()
+        ' Clear green grid on the table
+        For pedPeriodIndex As Period = Period.TUTORIAL To Period.PERIOD6 ' traverse all labels in the table
+            For rmmRoomIndex As Room = Room.S01 To Room.S10
+                ' Different color in odd and even number row
+                If pedPeriodIndex Mod 2 = 1 And linker.lblTableLinker(pedPeriodIndex, rmmRoomIndex).BackColor = Color.PaleGreen Then
+                    linker.lblTableLinker(pedPeriodIndex, rmmRoomIndex).BackColor = Color.WhiteSmoke
+                ElseIf linker.lblTableLinker(pedPeriodIndex, rmmRoomIndex).BackColor = Color.PaleGreen Then
+                    linker.lblTableLinker(pedPeriodIndex, rmmRoomIndex).BackColor = Color.Gainsboro
+                End If
+            Next
+        Next
+    End Sub
+
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
-        ' Clear the form
-        Clear()
+        ' Clear all text boxes, combo boxes and reset table
+        ' Clear text box
+        txtApplicantNameInput.Text = Nothing
+        txtPurposeInput.Text = Nothing
+        ' Clear combo box
+        cboClassroomChoose.SelectedItem = Nothing
+        cboTimeChoose.SelectedItem = Nothing
+        ' Clear the table
+        ClearTable()
     End Sub
 
     Private Sub btnSubmitRequest_Click(sender As Object, e As EventArgs) Handles btnSubmitRequest.Click
+        ' If the user leave one of Name, Purpose, Time and Room blank, then reject request and inform
+        If txtApplicantNameInput.Text Is Nothing Or txtPurposeInput.Text Is Nothing Or cboClassroomChoose.SelectedItem Is Nothing Or cboTimeChoose.SelectedItem Is Nothing Then
+            MsgBox("Please complete your Name, Purpose, Period and Room input", Title:="Order Rejected!")
+            Exit Sub
+        End If
+
         Dim strInputApplicantName As String
         Dim strInputPurpose As String
         Dim pedChosenPeriod As Period
@@ -201,8 +238,21 @@ Public Class MasterForm
                                                                       cldrChooseDate.SelectionEnd)
         Dim datUserChooseDate As Date = userSelectionRange.Start
 
+        ' Check if user choose another date after loading the timetable
+        If datCurrentChooseDate <> datUserChooseDate Then
+            MsgBox("Please load the timetable on your choosing date", Title:="Date Not Match Error")
+            Exit Sub
+        End If
+
+        ' Check if the room is allocated
+        Dim linker As LabelTableLinker = New LabelTableLinker
+        If linker.lblTableLinker(pedChosenPeriod, rmmChosenRoom).BackColor = Color.OrangeRed Then
+            MsgBox("The room is allocated. Order is Rejected", Title:="Room Already Allocated")
+            Exit Sub
+        End If
+
         ' Process data into .csv format
-        strCombinedData = strInputApplicantName & "," & strInputPurpose & "," & pedChosenPeriod & "," & rmmChosenRoom & vbCrLf
+        strCombinedData = strInputApplicantName & "," & strInputPurpose & "," & rmmChosenRoom & "," & pedChosenPeriod & vbCrLf
 
         ' Write into .csv file
         My.Computer.FileSystem.WriteAllText(
@@ -213,4 +263,13 @@ Public Class MasterForm
         MsgBox("Record Saved!")
     End Sub
 
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        MsgBox("How to use the system:" & vbCrLf & "Step 1, choose the date that you want for room booking" & vbCrLf & "Step 2, chooses the time and the classroom that is available or the system recommends." & vbCrLf & "Step 3, It is necessary to provide enough information. Such as name and purpose. The purpose of academics is acceptable." & vbCrLf & "Step 4, press the ‘submit’ button to send the order and the process of the booking is done." & vbCrLf & vbCrLf & "‘Clear’ button: Allow to use if the user needs to cancel the booking.",
+               Title:="Help Doc")
+    End Sub
+
+    Private Sub btnClearGreen_Click(sender As Object, e As EventArgs) Handles btnClearGreen.Click
+        ' Clear green grids
+        ClearGreenGrid()
+    End Sub
 End Class
